042acb6545ecb8dc8fe555204cbc0c8fa1b6dc7d
Canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-06-16 18:07:07 -0700

    Update view and controller

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,28 @@
 sap.ui.define(
-  ['sap/ui/core/mvc/Controller'],
+  ['sap/ui/core/mvc/Controller', 'sap/ui/core/format/DateFormat'],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
+   * @param {typeof sap.ui.core.format.DateFormat} DateFormat
    */
-  function (Controller) {
+  function (Controller, DateFormat) {
+    // Create DateFormat instance
+    const oDateFormat = DateFormat.getDateInstance({
+      pattern: 'MM/dd/yyyy',
+    });
+
     return Controller.extend('helloworld.controller.Main', {
       formatter: {
+        /**
+         * @param {Date} oDate
+         * @returns Formatted date
+         */
+        date: function (oDate) {
+          return oDate ? oDateFormat.format(oDate) : null;
+        },
+        /**
+         * @param {string} sStatus
+         * @returns Row highlight color
+         */
         highlightColor: function (sStatus) {
           switch (sStatus) {
             case 'E':
@@ -21,7 +38,12 @@ sap.ui.define(
           }
         },
       },
-      onInit: function () {},
+      getTable: function () {
+        return this.byId('table');
+      },
+      onRefresh: function () {
+        this.getTable().getBinding().refresh(true);
+      },
     });
   }
 );
